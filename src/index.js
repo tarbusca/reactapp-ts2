91dbf34481972f6430ed640a9f1aceac5aec9b44
tarbusca <66177633+tarbusca@users.noreply.github.com>
2020-06-07 01:48:04 -0400

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './mycss.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Hi({name, batch}) {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    return <div><center>
+      <h1>This Assignment # 3</h1>
+Prepared by <strong>{name}</strong>
+<br></br>From batch {batch}
+      <ol>
+        <li>Changed the Hello world</li>
+        <li>Made my name bold (see above) </li>
+        <li>Added more html tag e.g heading</li>
+        <li>Added the ordered list</li>
+        <li>Expression solving as well, ten plus five = {10+5}</li>
+      </ol> 
+      
+      <p className="myStyle"> This is styled text using css </p>
+    </center></div>;
+
+  }
+
+  ReactDOM.render(<Hi name="Tariq Saeed" batch="Block Chain 34"/>, document.querySelector('#root'));
