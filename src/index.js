1f87065c5624461c6207ce3dfbc61a735653ab91
tarbusca <66177633+tarbusca@users.noreply.github.com>
2021-06-27 01:47:27 -0400

    corrected table in index.js

--- src/index.js
+++ src/index.js
@@ -4,21 +4,38 @@ import './mycss.css';
 
 function Hi({name, batch}) {
 
-    return <div><center>
-      <h1>Panacloud Blockchain Practical Training</h1>
-    <h2>Step 7 - Transfer DAI </h2>
-Prepared by <strong>{name}</strong>
-<br></br>From batch {batch}
-
-  <label for="ethAddress">Transfer To Address:</label><br>
-  <input type="text" id="ethAddress" name="fname"><br>
-  <label for="amount">DAI Amount:</label><br>
-  <input type="text" id="amount" name="amount">
-  <button id='send'> Send </button>
-
-      
-      <p className="myStyle"><button>Connect to Metamask</button></p>
-    </center></div>;
+    return <div> <center>
+        <h1>Panacloud Blockchain Practical Training</h1>
+      <h2>Step 7 - Transfer DAI </h2>
+  Prepared by <strong>{name}</strong>
+   <br>From batch {batch}
+  <br>
+  <p className="myStyle"><button>Connect to Metamask</button></p>
+  <table> 
+  <tr align="left">
+    <td>
+        Tranfer To Address
+    </td>
+    <td>
+        <input type="text" id="ethAddress" name="fname">
+    </td>
+</tr> 
+<tr align="left"> 
+    <td>
+        DAI Amount
+    </td>
+    <td align="left">    
+        <input type="text" id="amount" name="amount">
+    </td>
+ </tr> 
+ <tr> 
+     <td></td>
+     <td align="right">  <button id='send'> Send </button> </td>
+</tr>
+   </table>     
+        
+     </center>
+    </div>; 
 
   }
 
